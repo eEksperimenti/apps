41c56b2bf0e2a7d63fa568e601e9efea60a0bf25
Samo <Samo@Samos-MacBook-Pro.local>
2015-12-16 15:41:07 +0100

    Onemogočen gumb med predvajanjem. Dodan html.

--- zvok.js
+++ zvok.js
@@ -1,122 +1,100 @@
-    $('#zvok').on('click', function(){
+$('#zvok1').popover({ trigger: "hover", html: true });
+$('#zvok2').popover({ trigger: "hover", html: true });
 
-      $('#zvok').attr("disabled", true);
+$('#zvok1').on('click', function(){
+  if(predvajanje != 1){
+    predvajanje = 1;
+    zvok(1);
+  }
+});
 
-      //var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+$('#zvok2').on('click', function(){
+  if(predvajanje != 1){
+    predvajanje = 1;
+    zvok(2);
+  }
+});
 
-      window.audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        
-      var t = [];
-      var min = 0;
-      var max = 0;
+function zvok(kanal){
 
-      for(var i = 0; i < 1024; i++){
-
-        t.push(datasets[0].data[i]);
-
-        if(t[i] < min){
-          min = t[i];
-        }
-        if(t[i] > max){
-          max = t[i];
-        }
-      }
-
-      var Nsignal = 1024;                      
-      var fmin = 200;
-      var fmax = 1200;
-       
-      var k = (fmax - fmin)/(max - min);
-      var n = fmax - k*max;
-
-      var z = 0.00; 
-      var časi = [];
-      var frekvence = [];
-      var fr;
-
-      for (var i = 0; i < 1024; i++){
-
-        fr = k*t[i]+n;
-        fr = Math.round(fr);
-
-        z = Math.round((z + 0.05)*100)/100;
+  window.audioContext = new (window.AudioContext || window.webkitAudioContext)();
       
-        časi.push(z);
-        frekvence.push(fr);
-      }
+  var min = parseFloat(params.local.min_y);
+  var max = parseFloat(params.local.max_y);
 
-      var oscillator = window.audioContext.createOscillator();
-      var gain =  window.audioContext.createGain();
-      gain.gain.value = 1;
+  var Nsignal = 1024;                      
+  var fmin = 200;
+  var fmax = 1200;
+   
+  var k = (fmax - fmin)/(Math.abs(max - min));
+  var n = fmax - k*Math.abs(max);
 
-      oscillator.connect(gain);
-      gain.connect(window.audioContext.destination);
-      oscillator.noteOn(0);
+  var frekvence = [];
+  var fr;
 
-      var i = 0;
+  for (var i = 0; i < 1024; i = i+2){   // zracunaj sample
 
-      var cs = setInterval( function(){
+    fr = k*parseFloat(datasets[kanal - 1].data[i][1]) + n;
+    fr = Math.round(fr*10000)/10000;
 
-        oscillator.frequency.value = frekvence[i];
-        console.log(frekvence[i]);
+    frekvence.push(fr);
+  }
 
-        if(i == 1024){
-          gain.gain.value = 0;
-          oscillator.noteOff();
-          clearInterval(cs);
-        }
-        i++;
+  var oscillator = window.audioContext.createOscillator();
+  var gain =  window.audioContext.createGain();
+  gain.gain.value = 1;
 
-      }, 5);
+  oscillator.connect(gain);
+  gain.connect(window.audioContext.destination);
+  oscillator.noteOn(0);
 
-      /*
-      window.updateSig = function(freq, z, k){
+  var i = 0;
 
-        oscillator.frequency.value = freq;
-        oscillator.connect(audioContext.destination);
-        oscillator.start(audioContext.currentTime + z);
-        oscillator.stop(audioContext.currentTime + z + k);
+  var cs = setInterval( function(){
 
-        if(z - 4 > 1 && z > 4 && z < 6 && !isNaN(z) && isFinite(z)){
-          $('#zvok').attr("disabled", false);
-        }
-      };
+    oscillator.frequency.value = frekvence[i];
+    
+    if(i == 512){
+      gain.gain.value = 0;
+      predvajanje = 0;
+      clearInterval(cs);
+    }
+    i++;
 
-      var t = datasets[0].data.slice();
-      var min = 0;
-      var max = 0;
+  }, 5);
+}
 
-      for(var i = 0; i < 1024; i++){
-
-        t[i][1] = t[i][1]*100;
-        
-        if(t[i][1] < min){
-            min = t[i][1];
-        }
-        if(t[i][1] > max){
-          max = t[i][1];
-        }
-      }
-
-      var fmin = 400;
-      var fmax = 2000;
-       
-      var k = (fmax - fmin)/(max - min);
-      var n = fmax - k*max;
-
-      var z = 0; 
-      var fr = 0;
-
-      var oscillator = audioContext.createOscillator();
-      oscillator.type = 'sine';
-      
-      for (var i = 0; i < 1024; i = i + 10){
-
-        fr = k*t[i][1] + n;
-        fr = Math.round(fr);
-
-        z = z + 0.04;
-      
-        window.updateSig(fr, z, 0.04);    //predvajanje
-      }*/
-  });
+<div id="btn_toolbar" class="col-xs-12">
+  <div class="col-xs-8">
+    <div class="col-xs-4">
+      <div id="selzoompan" class="btn-group" data-toggle="buttons">
+        <button id="btn_zoomin" class="btn btn-primary" onclick="selectTool.call(this, 'zoomin')">
+        <span class="glyphicon glyphicon-zoom-in"></span>
+        </button>
+        <button id="btn_zoomout" class="btn btn-default" onclick="selectTool.call(this, 'zoomout')">
+        <span class="glyphicon glyphicon-zoom-out"></span>
+        </button>
+        <button id="btn_pan" class="btn btn-default" onclick="selectTool.call(this, 'pan')" style="">
+        <span class="glyphicon glyphicon-move"></span>
+        </button>
+      </div>
+    </div>
+    <div class="col-xs-4">
+      <button id="btn_ch1" class="btn btn-primary" data-checked="true" onclick="setVisibleChannels(this)">Kanal 1</button>
+      <button id="btn_ch2" class="btn btn-primary" data-checked="true" onclick="setVisibleChannels(this)">Kanal 2</button>
+    </div>
+    <div class="col-xs-4" style="padding-bottom: 0.5em;">
+      <div class="col-xs-6">
+        <button id="zvok1" style="background-color: white; border: none;" data-content="S klikom na ta gumb, se signal na zaslonu pretvori v zvočni signal z uporabo pretvorbe iz amplitude v frekvenco (amplitudno frekvenčna modulacija). To je eksperimentalni sistem, ki je namenjen slabovidnim ali slepim, ki jim ta način morda lahko olajša ali sploh omogoča sodelovanje pri eksperimentih. Sinusni signal se torej ne sliši kot zvok ene frekvence pač pa kot izmenjaje višji in nižji toni. <br><br> Aplikacija deluje smiselno, če graf na sliki miruje. To dosežemo z ustrezno nastavitvijo Proženja signala." data-placement="bottom">
+        <img class="img-responsive" src="../assets/images/zvok_simbol_ch1.png" alt="zvok1" style="width: 100%;"/>
+        </button>
+      </div>
+      <div class="col-xs-6">
+        <button id="zvok2" style="background-color: white; border: none;" data-content="S klikom na ta gumb, se signal na zaslonu pretvori v zvočni signal z uporabo pretvorbe iz amplitude v frekvenco (amplitudno frekvenčna modulacija). To je eksperimentalni sistem, ki je namenjen slabovidnim ali slepim, ki jim ta način morda lahko olajša ali sploh omogoča sodelovanje pri eksperimentih. Sinusni signal se torej ne sliši kot zvok ene frekvence pač pa kot izmenjaje višji in nižji toni. <br><br> Aplikacija deluje smiselno, če graf na sliki miruje. To dosežemo z ustrezno nastavitvijo Proženja signala." data-placement="bottom">
+        <img class="img-responsive" src="../assets/images/zvok_simbol_ch2.png" alt="zvok2" style="width: 100%;"/>
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-4"></div>
+</div>
